bf4aaf631103c703a02c04aaf39d63fe488ea5ac
DanielWar01 <danielalejoguerra@gmail.com>
2024-11-28 18:14:34 -0500

    Initial commit

---
+++ src/test/java/co/edu/uptc/springbootexam/SpringbootExamApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.springbootexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
